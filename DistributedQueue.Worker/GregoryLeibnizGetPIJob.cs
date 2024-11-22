997d51b14e5e8a09fc403fda28cbd1f2bbe895cc
janlex02 <>
2024-11-23 00:39:22 +0300

    first commit

---
+++ DistributedQueue.Worker/GregoryLeibnizGetPIJob.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DistributedQueue.Common
+{
+    internal class GregoryLeibnizGetPIJob : IComputePiJob
+    {
+        public Task ComputePyAsync(string name, int iterrations, CancellationToken token)
+        {
+
+            var startTime = DateTime.Now;
+            string[] strings = {
+                    "Black",
+                    "DarkBlue",
+                    "DarkGreen",
+                    "DarkCyan",
+                    "DarkRed",
+                    "DarkMagenta",
+                    "DarkYellow",
+                    "Gray",
+                    "DarkGray",
+                    "Blue",
+                    "Green",
+                    "Cyan",
+                    "Red",
+                    "Magenta",
+                    "Yellow",
+                    "White"
+                };
+            int color = 0;
+            Random r = new Random();
+            for (int i = 0; i < iterrations; i++)
+            {
+                
+                Console.ForegroundColor = (ConsoleColor)color;
+                Console.WriteLine($"{DateTime.Now}: Compute task: {name} Now {strings[i%16]} on iterration: {i}");
+                color++;
+                if(color > 15) color = 0;
+
+                Thread.Sleep(50);
+            }
+            
+            Console.ForegroundColor = (ConsoleColor)15;
+            Console.WriteLine("################################");
+            Console.WriteLine("################################");
+            Console.ForegroundColor = (ConsoleColor)9;
+            Console.WriteLine("################################");
+            Console.WriteLine("################################");
+            Console.ForegroundColor = (ConsoleColor)12;
+            Console.WriteLine("################################");
+            Console.WriteLine("################################");
+            Console.ForegroundColor = (ConsoleColor)15;
+            Console.WriteLine($"Nane potoka: {name}, Iterrations: {iterrations}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
